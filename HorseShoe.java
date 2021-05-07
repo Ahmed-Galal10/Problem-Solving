27e3097c35bf26ef19cbf1f641c04fe4c4d82fd8
Ahmed Galal <ahmedgalal10895@gmail.com>
2021-05-07 04:30:09 +0200

    Add new solved problem 'HorseShoe'

---
+++ HorseShoe.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+public class HorseShoe{
+
+    public static void main(String[] args){
+        
+        Set<Integer> shoesSet = new HashSet<>();
+        
+        Scanner input = new Scanner(System.in);
+        
+        for(int i = 0; i < 4; i++){
+            shoesSet.add(input.nextInt());
+        }
+      
+        int shoesNeeded = 4 - shoesSet.size();
+        
+        System.out.println(shoesNeeded);
+    }
+}
