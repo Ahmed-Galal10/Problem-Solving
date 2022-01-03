95c3fd74d201842064f851d733ae6da69e25ca82
Ahmed Galal <ahmedgalal10895@gmail.com>
2022-01-03 16:36:14 +0200

    Add solved one " Palindrome Number "

--- leetCode/PalindromeNumber.java
+++ leetCode/PalindromeNumber.java
@@ -1,16 +1,15 @@
 class PalindromeNumber {
     public boolean isPalindrome(int num) {
-        
+        //let num = 131
         int inputNum = num;
         int reverse = 0;
         
         while(num > 0){
             reverse = (reverse * 10) + num % 10;
+            //1 - 13 - 131
             num /= 10;
+            //13 - 1 - 0
         }
-        
         return (inputNum == reverse);
-        
     }
-    
 }
