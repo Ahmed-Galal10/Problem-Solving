5ac7d683587f276fe1f10639b82ae6968ed380dd
Ahmed Galal <ahmedgalal10895@gmail.com>
2022-01-07 23:50:31 +0200

    Add solved one " Goal Parser Interpretation"

---
+++ leetCode/GoalParser.java
@@ -0,0 +1,22 @@
+class GoalParser {
+    public String interpret(String command) {
+        StringBuilder commandParser = new StringBuilder();
+        
+        for(int i = 0; i < command.length(); i++){
+            if (command.charAt(i) == '('){
+                if (command.charAt(i+1) == ')'){
+                    commandParser.append('o');
+                    i++;
+                }else {
+                    commandParser.append("al");
+                    i += 3;
+                }
+            } else {
+                commandParser.append(command.charAt(i));
+            }
+        }
+        
+        return commandParser.toString();
+		// or return command.replace("()", "o").replace("(al)", "al");
+    }
+}
