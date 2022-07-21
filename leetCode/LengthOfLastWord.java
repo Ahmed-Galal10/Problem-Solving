90aa770451ebb76b20cc3121d0185bd8afbd0eb5
Ahmed Galal <ahmedgalal10895@gmail.com>
2022-07-22 00:42:57 +0200

    Add solved one " Length Of Last Word "

---
+++ leetCode/LengthOfLastWord.java
@@ -0,0 +1,8 @@
+class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        String[] words = s.split(" ");
+        String lastWord = words[words.length - 1];
+
+        return lastWord.length();
+    }
+}
