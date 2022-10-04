efbeeb4db6f0404177684e5be23b08270b612dd3
Ahmed Galal <ahmedgalal10895@gmail.com>
2022-10-05 00:27:16 +0200

    Add other solution for "Two Sum" problem

---
+++ leetCode/TwoSum2.java
@@ -0,0 +1,19 @@
+class TwoSum2{
+    public int[] twoSum(int[] nums, int target) {
+        int[] twoSumIndexes = new int[2];
+
+        HashMap<Integer, Integer> numbersMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!numbersMap.containsKey(target - nums[i])) {
+                numbersMap.put(nums[i], i);
+            } 
+            else {
+                twoSumIndexes[0] = numbersMap.get(target - nums[i]);
+                twoSumIndexes[1] = i;
+                break;
+            }
+        }
+        return twoSumIndexes;
+    }
+}
+}
