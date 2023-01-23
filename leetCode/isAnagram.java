c5a2676c3f8ed1cf73cf719ab1f600a053fdf002
Ahmed Galal <ahmedgalal10895@gmail.com>
2023-01-23 23:53:24 +0200

    Add solved problem " Valid Anagram "

--- leetCode/isAnagram.java
+++ leetCode/isAnagram.java
@@ -4,17 +4,15 @@ class Solution {
         for (char c : s.toCharArray()) {
             alphaChars[c - 'a']++;
         }
-
         for (char c : t.toCharArray()) {
             alphaChars[c - 'a']--;
         }
-
+		
         for (int n : alphaChars) {
             if (n != 0){
                 return false;
             }
         }
-
         return true;
     }
 }
